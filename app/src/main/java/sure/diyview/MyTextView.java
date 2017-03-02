ac5eae850b2b3966e49a8c3d84ddb64d61d5d42c
suReZj <1014652997@qq.com>
2017-03-02 11:04:49 +0800

    first commit

---
+++ app/src/main/java/sure/diyview/MyTextView.java
@@ -0,0 +1,56 @@
+package sure.diyview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by dell88 on 2017/3/2 0002.
+ */
+
+public class MyTextView extends TextView {
+    private int mViewWidth=0;
+    private int mTranslate=0;
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private Paint mPaint;
+
+    public MyTextView(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+    @Override
+    protected void onSizeChanged(int w,int h,int oldw,int oldh){
+        super.onSizeChanged(w,h,oldw,oldh);
+        if(mViewWidth==0){
+            mViewWidth=getMeasuredWidth();
+            if(mViewWidth>0){
+                mPaint=getPaint();
+                mLinearGradient=new LinearGradient(0,0,mViewWidth,0,new int[]{
+                        Color.BLUE,0xffffffff,
+                        Color.BLUE
+                },null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix=new Matrix();
+            }
+        }
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mGradientMatrix != null) {
+            mTranslate += mViewWidth / 5;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
